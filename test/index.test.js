e3bb4e71807592a35c1e85918eb55bc04f28084c
asawadichai <sawadichai.andrew@gmail.com>
2022-08-03 19:16:08 -0700

    testing pushing code into github

--- test/index.test.js
+++ test/index.test.js
@@ -5,3 +5,7 @@ const sum = require('../database/index.js');
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test('2 + 2 to equal 4', () => {
+  expect(sum(2, 2).toBe(3));
+})
